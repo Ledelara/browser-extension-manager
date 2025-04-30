0c64128ccfa22e329924acb2d6f183abc4f30591
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-04-30 20:16:14 -0300

    🚚 move: movendo assets para o diretório public

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { ThemeProvider } from "styled-components";
+import { theme } from "./components/StyledComponents/globalStyle";
+import "./App.css";
+import { Button } from "./components/StyledComponents/Button/Button";
+import HeaderComponent from "./components/Header/Header";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <header>
+        <HeaderComponent />
+      </header>
+      <div>
+        <h1>Browser Extension Manager</h1>
+        <Button primary>Botão principal</Button>
+        <Button>Botão secundário</Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
